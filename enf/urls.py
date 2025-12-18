63eb0eb10a6713c9bce29bc7f993b3edf9c0a32c
Bogdan <152705351+skyfi3@users.noreply.github.com>
2025-12-18 15:47:34 +0200

    orders and payment before logging in

--- enf/urls.py
+++ enf/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('users/', include('users.urls', namespace='users')),
-    path('', include('main.urls', namespace='main')),
+    path("admin/", admin.site.urls),
+    path("cart/", include("cart.urls", namespace="cart")),
+    path("users/", include("users.urls", namespace="users")),
+    path("orders/", include("orders.urls", namespace="orders")),
+    path("payment/", include("payment.urls", namespace="payment")),
+    path("", include("main.urls", namespace="main")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
